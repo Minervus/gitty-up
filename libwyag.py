8d7c9a46c6a25ce6949d0a0738f7fcd3be80aca0
Tony Nguyen <tnguyen411@gmail.com>
2025-04-24 13:18:46 -0700

    working on git object definition

--- libwyag.py
+++ libwyag.py
@@ -25,3 +25,28 @@ def main(argv=sys.argv[1:]):
         case "add"  : cmd_add(args)
         case "cat-file" : cmd_cat_file(args)
         case "check-ignore" : cmd_check_ignore(args)
+        case "checkout" : cmd_commit(args)
+        case "hash-object" : cmd_hash_object(args)
+        case "init" : cmd_init(args)
+        case "log" : cmd_log(args)
+        case "ls-files" : cmd_ls_files(args)
+        case "ls-tree"  : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"   : cmd_rm(args)
+        case "show-ref" : cmd_show_ref(args)
+        case "status"   : cmd_status(args)
+        case "tag"  : cmd_tag(args)
+        case _ : print("Bad command.")
+
+class GitRepository (object):
+    """A git repository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        
