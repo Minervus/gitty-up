fc10c0f7f893a4785f0bca298ea80a6522023f29
Tony Nguyen <tnguyen411@gmail.com>
2025-04-28 17:09:02 -0700

    adding Git repo object

--- libwyag.py
+++ libwyag.py
@@ -45,8 +45,26 @@ class GitRepository (object):
     gitdir = None
     conf = None
 
+
+    # repo init function, force = optional argument that disables all checks
     def __init__(self, path, force=False):
         self.worktree = path
         self.gitdir = os.path.join(path, ".git")
 
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git repository {path}")
         
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+        
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(f"Unsupported repositoryformatversion: {vers}")
+
